2ca2de1305f16d63f803825fb064680e0190a102
Sam Byrne <ssbyrne@uw.edu>
2022-03-13 19:26:01 -0700

    added data files and put in correct folder

---
+++ R/data.R
@@ -0,0 +1,34 @@
+#' modeling_hierarchy
+#'
+#' Example hierarchy for function development
+#'
+#' @format A data.frame with 465 rows and 29 variables:
+#'
+"modeling_hierarchy"
+
+
+#' covariate_with_aggregates_hierarchy
+#'
+#' Example hierarchy for function development
+#'
+#' @format A data.frame with 516 rows and 29 variables:
+#'
+"covariate_with_aggregates_hierarchy"
+
+
+#' fh_small_area_hierarchy
+#'
+#' Example hierarchy for function development
+#'
+#' @format A data.frame with 305 rows and 29 variables:
+#'
+'fh_small_area_hierarchy'
+
+
+#' gbd_analysis_hierarchy"
+#'
+#' Example hierarchy for function development
+#'
+#' @format A data.frame with 1126 rows and 31 variables:
+#'
+"gbd_analysis_hierarchy"
