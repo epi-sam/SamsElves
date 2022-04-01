422a8b52a2c8ab6c307edf40cf9d6ea1e1718c8d
Sam Byrne <ssbyrne@uw.edu>
2022-03-31 22:54:04 -0700

    updated documentation for preflight_checks

--- R/preflight_checks.R
+++ R/preflight_checks.R
@@ -1,80 +1,23 @@
-# TODOs -------------
-
-# - [ ]
-#   - [ ]
-
-# Now
-# - [x] Allow for column renaming
-# - [x] Limit data2data comparisons to unique values in each
-# - [x] Allow for filtering as well as column selection
-#   - [x] Can I allow for multiple filters?
-# - [x] Add a top-level function to select, rename and filter data.frames
-# - [x] define tidyverse functions to avoid require() or library()
-# - [X] Add 'stop' argument
-# - [x] Incorporate switch() for 'method' argument
-# - [x] Write the "stop" chunk as a generalizable function
-#   - [x] main chunk
-#   - [x] needs a stop-condition argument, or else it stops every time
-# - [ ] read through all steps and align documentation notes with actual processes
-# - [ ] more toture testing, especially for stop_conditions
-
-# This is still a bit too cumbersome
-# - [x] rename 'data2data' as a more generic function, which it is
-# - [x] create a true heirarchy to dataset function, with assumed inputs (pretty safe)
-# - [x] hier2hier function
-# - [x] column check function
-
-# Later
-# - [ ] look into tryCatch()
-# - [ ] look at Tom's shapefiles
-
-# "main" = "/ihme/covid-19/shapefiles/covid_simp_2.shp",
-# Contains French Guiana 143, New Caledonia 228, Western Sahara 313
-# "unmodeled" = "/home/j/DATA/SHAPE_FILES/GBD_geographies/master/GBD_2016/inset_maps/noSubs/GBD_WITH_INSETS_NOSUBS.shp",
-# For India special Jammu & Kashmir and Ladakh 4854
-# "india_disputed" = "/home/j/WORK/11_geospatial/admin_shapefiles/current/IND_full_ad1.shp",
-# "india_disputed_mask" = "/home/j/WORK/11_geospatial/admin_shapefiles/2020_05_21/lbd_disputed_mask.shp",
-# "ind_lalit" = "/ihme/covid-19/shapefiles/india_shp/STATE_2020.shp",
-# "mainland_china" = "/snfs1/DATA/SHAPE_FILES/GBD_geographies/master/GBD_2020/master/shapefiles/GBD2020_mapping_final.shp",
-# "hong_kong_macao" = "/home/j/DATA/SHAPE_FILES/GBD_geographies/master/covid/20210210/covid_clean.shp"
-
-
-# --- prep environment for development work ---
-rm(list = ls(all.names = T))
-source("R/env_setup_for_package_dev.R")
-
-
-# FUNCTION BODY ------
 #' Compare reference vectors or data.frames using Tidyverse grammar
 #'
-#' Returns a list of diagnostic information.  I recommend saving this output
-#' to an object with a name that is diagnostically helpful if you set STOP=FALSE
-#'
-#' 'X' is intended as the left-side, 'reference' data - that which is your gold standard to compare against
-#' 'Y' is intended as the rigth-side 'comparison' data - that which you want to validate AGAINST X
-#'
-#' This function may STOP a script if specified.
-#'
-#' Many 'methods' of comparison are available - check function arguments.
-#'
-#' Suppose your pipeline depends on having data for all locations in a given
-#' hierarchy. Suppose you need to compare two hierarchies directly for equality
-#' across a range of columns (c("location_id", "location_name",
-#' "path_to_top_parent", "most_detailed") are defaults). This functions takes a
-#' left (X) and right (Y) vector or data.frame as input, then compares them
-#' according to a desired comparison method.
-#'
-#' If you need to compare data.frame columns with the same content but different
-#' column names, enter those names in the same order in colsX and colsY
-#' arguments, and the function will use colsX (left-hand data.frame) to rename
-#' all columnn, and output will return those names.
-#'
-#' Filtering occurs AFTER column selection.  Format all filters according to colsX names.
-#'
-#' Order of operations:
-#' select()
-#' setNames()
-#' filter()
+#' Returns a list of diagnostic information.  I recommend saving this output to
+#' an object with a name that is diagnostically helpful if you set STOP=FALSE.
+#' 'X' is intended as the left-side, 'reference' data - that which is your gold
+#' standard to compare against 'Y' is intended as the rigth-side 'comparison'
+#' data - that which you want to validate AGAINST X This function may STOP a
+#' script if specified. Many 'methods' of comparison are available - check
+#' function arguments. Suppose your pipeline depends on having data for all
+#' locations in a given hierarchy. Suppose you need to compare two hierarchies
+#' directly for equality across a range of columns (c("location_id",
+#' "location_name", "path_to_top_parent", "most_detailed") are defaults). This
+#' functions takes a left (X) and right (Y) vector or data.frame as input, then
+#' compares them according to a desired comparison method. If you need to
+#' compare data.frame columns with the same content but different column names,
+#' enter those names in the same order in colsX and colsY arguments, and the
+#' function will use colsX (left-hand data.frame) to rename all columnn, and
+#' output will return those names. Filtering occurs AFTER column selection.
+#' Format all filters according to colsX names. Order of operations: select(),
+#' setNames(), filter()
 #'
 #' @param X Left-hand input vector or data.frame (gold standard to compare against)
 #' @param Y Right-hand input vector or data.frame (compared AGAINST your gold standard, by 'method')
@@ -99,8 +42,8 @@ source("R/env_setup_for_package_dev.R")
 #'
 #' @examples
 #'
-#' library(dplyr)
-#' source("/ihme/cc_resources/libraries/current/r/get_location_metadata.R")
+#'library(dplyr)
+#'source("/ihme/cc_resources/libraries/current/r/get_location_metadata.R")
 #'
 #'MREs -----------------
 #'source("/ihme/cc_resources/libraries/current/r/get_location_metadata.R")
@@ -203,6 +146,7 @@ preflight_checks <- function(
       # if a filter statement exists, use it, otherwise include all rows
       filter(ifelse(!is.null(filter_statement), eval(parse(text = filter_statement)), all_of(colsX[1] %in% colsX[1])))
     return(X)
+
   }
 
   prep_dataY <- function(Y = Y, colsY = colsY, filter_statement = filter_statement){
@@ -235,7 +179,6 @@ preflight_checks <- function(
       # print(Out_list)
       message("Passed the stop condition - continuing script.")
       return(Out_list) # return verbose to console and for assignment
-
     } else if (!stop_condition & !verbose) {
       message("Passed the stop condition - continuing script.")
       invisible(Out_list) # return invisibly for assignment
@@ -264,7 +207,7 @@ preflight_checks <- function(
     stop_or_continue(STOP = STOP, method = method, Out_list = Out_list,
                      stop_condition = !isTRUE(all.equal(X,Y)), # must be added for each method
                      helpful_message ="Dataframes are not all equal, see above.")
-    # }
+
   }
 
   # Method 2 : data2data ----------------------------
@@ -292,7 +235,6 @@ preflight_checks <- function(
                      stop_condition = nrow(setdiff(distinct(X), distinct(Y))) > 0,
                      helpful_message = "There is a difference between data.frames")
 
-    # return(Out_list)
   }
 
   # Method 3 : hier2data  ----------------------------
@@ -314,8 +256,6 @@ preflight_checks <- function(
     stop_or_continue(STOP = STOP, method = method, Out_list = Out_list,
                      stop_condition = length(setdiff(X$location_id, Y$location_id)) > 0,
                      helpful_message = "Not all locations in X are present in Y")
-
-    # return(Out_list)
 
   }
 
@@ -384,93 +324,3 @@ preflight_checks <- function(
   )
 
 }
-
-
-# SANDBOX -----------------
-
-# MREs -----------------------------
-
-source("/ihme/cc_resources/libraries/current/r/get_location_metadata.R")
-hier_covid_771 <- getloc
-
-
-# Vetting -----------------------
-X <- hier_covid
-Y <- copy(X)
-Y <- hier_gbd
-thing1 <- hier_covid %>% select(location_id, path_to_top_parent)
-thing2 <- hier_covar %>% select(location_id, path_to_top_parent)
-setdiff(union(thing1, thing2), intersect(thing1, thing2))
-# setdiff(thing1, thing2)
-# setdiff(thing2, thing1)
-X %>% left_join(Y, by = c("location_id", "path_to_top_parent"))
-
-# compare_cols
-preflight_checks(Equal1, Equal2, method = "compare_cols")
-preflight_checks(Equal1, Equal2, method = "compare_cols", STOP = T)
-preflight_checks(Diff1, Diff2, method = "compare_cols", STOP = F)
-preflight_checks(Diff1, Diff2, method = "compare_cols", STOP = T)
-
-#hier2hier
-preflight_checks(hier_covid, hier_covid, method = "hier2hier")
-
-# hier2data
-
-preflight_checks(hier_covid, full_data, method = "hier2data", STOP = F)
-preflight_checks(hier_covid, hier_covid, method = "hier2data", STOP = F)
-hier_covid %>% filter(location_id %in% setdiff(hier_covid$location_id, full_data$location_id) ) %>%  select(location_id, location_name)
-
-
-# data2data
-X <- Diff1
-Y <- Diff2
-colsX <- c("location_id", "path_to_top_parent")
-
-
-preflight_checks(Diff1, Diff2, method = "data2data", colsX = "location_id")
-preflight_checks(Diff1, Diff2, method = "data2data", colsX = c("location_id", "path_to_top_parent"))
-
-preflight_checks(Equal1, Equal2, method = "data2data", colsX = c("location_id", "path_to_top_parent"))
-
-hier_check <- hier_covid %>% filter(most_detailed==1)
-check270 <- preflight_checks(hier_check, full_data, method = 'data2data', colsX = c("location_id"), STOP = F)
-xlocs <- ERRORS_data2data$in_X_not_Y
-ylocs <- ERRORS_data2data$in_Y_not_X
-hier_check %>% filter(location_id %in% xlocs$location_id)
-ylocsnames <- full_data %>% filter(location_id %in% ylocs$location_id) %>% distinct(location_id, location_name)
-
-check270 <- preflight_checks(full_data, full_data, method = 'data2data', colsX = c("location_id"), STOP = T)
-
-# still a bit cumbersome to use...
-
-
-# all_equal
-# naive, no method specified
-preflight_checks(Equal1, Equal2, STOP=F)
-preflight_checks(Equal1, Equal2, STOP=T)
-preflight_checks(Diff1, Diff2, method = "all_equal", STOP = F)
-preflight_checks(Diff1, Diff2, method = "all_equal", STOP = T)
-
-
-# early column mismatch
-preflight_checks(X = Diff2,
-                 Y = Diff1,
-                 method = "data2data",
-                 colsX = c("location_id", "lancet_label")) # should stop
-preflight_checks(X = Diff2,
-                 Y = Diff1,
-                 method = "data2data",
-                 colsX = c("location_id", "location_name")) # should continue with warning
-
-
-# --- test some things ---
-
-# lots of output, maybe too much
-all.equal(HIER$covariate_with_aggregates_hierarchy$location_id, HIER$gbd_analysis_hierarchy$location_id)
-# output if equal?
-all.equal(equal1, equal2) #TRUE
-
-setdiff(names(HIER$covariate_with_aggregates_hierarchy), names(HIER$gbd_analysis_hierarchy))
-setdiff(names(HIER$gbd_analysis_hierarchy), names(HIER$covariate_with_aggregates_hierarchy))
-setdiff(names(h_covid), names(h_gbd))
-setdiff(names(h_gbd), names(h_covid))
