4fb5acb132ad7e542c4adf3349c974e94181bd8a
Sam Byrne <ssbyrne@ihme.washington.edu>
2023-01-04 12:06:44 -0800

    added function to prep a windows file path and return with correct format

---
+++ R/prep_path.R
@@ -0,0 +1,20 @@
+#' Convert windows file paths
+#'
+#' If you've copied a file path to the windows clipboard, call this function
+#' without any arguments to both return a path with correct slashes, as well as
+#' copy that path to the clipboard for an easy paste.
+#'
+#' @param path [character] a filepath to convert
+#'
+#' @return correctly formatted filepath (`\` becomes `/`)
+prep_path <- function(path = "clipboard") {
+  y <- if (path == "clipboard") {
+    readClipboard()
+  } else {
+    cat("Please enter the path:\n\n")
+    readline()
+  }
+  x <- chartr("\\", "/", y)
+  writeClipboard(x)
+  return(x)
+}
