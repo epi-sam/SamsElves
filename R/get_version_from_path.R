48dd5258a47b20991486ebaec3fb45ce52b86754
Sam Byrne <ssbyrne@uw.edu>
2023-02-15 09:30:27 -0800

    added a couple small utils - msg_prt and get-version-from-path

---
+++ R/get_version_from_path.R
@@ -0,0 +1,17 @@
+#' Get YYYY_MM_DD.VV version from an output path
+#' 
+#' Really just retuns the last element of a string split on "/".
+#' This function currently has no validation or safety rails
+#'
+#' @param x [file path] versioned output folder
+#'
+#' @return [char] "YYYY_MM_DD.VV" folder name
+#' @export
+#'
+#' @examples
+get_version_from_path <- function(x) {
+  
+  x <- unlist(strsplit(x, '/')) 
+  return(x[length(x)])
+  
+}
