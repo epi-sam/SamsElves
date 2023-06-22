b8f7aded599cf70ef3e53c7892273286d2d07977
Sam Byrne <ssbyrne@uw.edu>
2023-06-21 21:31:01 -0700

    testing function

--- R/define_roots.R
+++ R/define_roots.R
@@ -7,12 +7,19 @@
 #'
 #' @examples
 define_git_root <- function(){
+  INTERACTIVE <- rstudioapi::isAvailable()
   
-  if(interactive()){
-    git_root <- rprojroot::is_git_root$find_file()
+  git_root <- rprojroot::is_git_root
+  
+  if(INTERACTIVE){
+    this_file <- rstudioapi::getSourceEditorContext()$path
+    this_file <- DescTools::SplitPath(this_file)$fullfilename
   } else {
     stop("This is only reliable with interactive sessions - pass code_root to submitted jobs through arguments.")
   }
+  
+  git_root$find_file()
+  git_root <- rprojroot::find_root(this_file)
   
   message("Repo root is ", git_root)
   
