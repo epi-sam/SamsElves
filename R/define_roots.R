062a0777083e990a6f6357db3c8ff1b5db59e066
Sam Byrne <ssbyrne@uw.edu>
2023-09-21 10:58:16 -0700

    define roots - bugfixed define git repo root

--- R/define_roots.R
+++ R/define_roots.R
@@ -9,17 +9,12 @@
 define_git_repo_root <- function(){
   INTERACTIVE <- rstudioapi::isAvailable()
   
-  git_root <- rprojroot::is_git_root
   
-  if(INTERACTIVE){
-    this_file <- rstudioapi::getSourceEditorContext()$path
-    this_file <- DescTools::SplitPath(this_file)$fullfilename
-  } else {
-    stop("This is only reliable with Rstudio interactive sessions - pass code_root to submitted jobs through arguments.")
-  }
+  git_root_criterion <- rprojroot::is_git_root
   
-  git_root$find_file()
-  git_root <- rprojroot::find_root(this_file)
+  if(!INTERACTIVE) stop("This is only reliable with Rstudio interactive sessions - pass code_root to submitted jobs through arguments.")
+  
+  git_root <- rprojroot::find_root(git_root_criterion)
   
   message("Repo root is ", git_root)
   
