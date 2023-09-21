f8f838eb6eeb1357776b47072b1a4e9459621161
Sam Byrne <ssbyrne@uw.edu>
2023-09-21 11:00:14 -0700

    define roots - tweaked docstrings and error

--- R/define_roots.R
+++ R/define_roots.R
@@ -1,6 +1,6 @@
 #' Find the root directory of a git repository.
 #' 
-#' Intended use is from primary launch script.
+#' Only reliable with Rstudio interactive sessions.
 #'
 #' @return [path] path to root folder
 #' @export
@@ -9,10 +9,9 @@
 define_git_repo_root <- function(){
   INTERACTIVE <- rstudioapi::isAvailable()
   
-  
   git_root_criterion <- rprojroot::is_git_root
   
-  if(!INTERACTIVE) stop("This is only reliable with Rstudio interactive sessions - pass code_root to submitted jobs through arguments.")
+  if(!INTERACTIVE) stop("This is only reliable with Rstudio interactive sessions.")
   
   git_root <- rprojroot::find_root(git_root_criterion)
   
