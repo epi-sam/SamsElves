1ddece20363c4357696134711ce4d687139fc4dc
Sam Byrne <ssbyrne@uw.edu>
2023-09-22 10:17:14 -0700

    msg_prt - refined tests for expected input errors

--- tests/testthat/test-msg_prt.R
+++ tests/testthat/test-msg_prt.R
@@ -1,23 +1,10 @@
-test_that("input is a string", {
-  string <- "55"
-  expect_type(string, "character")
-})
-
-test_that("input is not a string", {
-  string <- 55L
-  expect_false(is.character(string))
-  string <- 55
-  expect_false(is.character(string))
-  string <- TRUE
-  expect_false(is.character(string))
-  string <- NA
-  expect_false(is.character(string))
-  string <- NaN
-  expect_false(is.character(string))
-  string <- NULL
-  expect_false(is.character(string))
-  string <- Inf
-  expect_false(is.character(string))
+test_that("expect input error", {
+  expect_error(msg_prt(TRUE, "both"))
+  expect_error(msg_prt(55L, "both"))
+  expect_error(msg_prt(NA, "both"))
+  expect_error(msg_prt(NaN, "both"))
+  expect_error(msg_prt(NULL, "both"))
+  expect_error(msg_prt(Inf))
 })
 
 test_that("output is a message", {
@@ -29,7 +16,6 @@ test_that("output is std_out", {
 })
 
 test_that("output is message and std_out", {
-  expect_output(msg_prt("My msg & print", output = "both"))
-  expect_message(msg_prt("My msg & print", output = "both"))
+    expect_output(msg_prt("My msg & print", output = "both"))
+    expect_message(msg_prt("My msg & print", output = "both"))
 })
-
