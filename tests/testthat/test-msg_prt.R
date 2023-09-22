372d1bbf0ff9af99a21416dd46e53ba66dd29bb7
Sam Byrne <ssbyrne@uw.edu>
2023-09-22 10:10:42 -0700

    msg_prt - updated to HIV SDG package implementation, added basic tests

--- tests/testthat/test-msg_prt.R
+++ tests/testthat/test-msg_prt.R
@@ -1,3 +1,35 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("input is a string", {
+  string <- "55"
+  expect_type(string, "character")
 })
+
+test_that("input is not a string", {
+  string <- 55L
+  expect_false(is.character(string))
+  string <- 55
+  expect_false(is.character(string))
+  string <- TRUE
+  expect_false(is.character(string))
+  string <- NA
+  expect_false(is.character(string))
+  string <- NaN
+  expect_false(is.character(string))
+  string <- NULL
+  expect_false(is.character(string))
+  string <- Inf
+  expect_false(is.character(string))
+})
+
+test_that("output is a message", {
+  expect_message(msg_prt("My message", output = "message"))
+})
+
+test_that("output is std_out", {
+  expect_output(msg_prt("My std_out", output = "print"))
+})
+
+test_that("output is message and std_out", {
+  expect_output(msg_prt("My msg & print", output = "both"))
+  expect_message(msg_prt("My msg & print", output = "both"))
+})
+
