38e9e5bbfb6a098ef4f79ce9ebf7126c3c6511c3
Sam Byrne <ssbyrne@uw.edu>
2023-11-15 12:56:01 -0800

    make_directory - added test fixture

--- R/utils_io.R
+++ R/utils_io.R
@@ -1,14 +1,14 @@
-#' SDG core function - Convenience `dir.create` wrapper with desired defaults
+#' Convenience `dir.create` wrapper with desired defaults
 #' 
 #' `dir.create()` natively prevents overwrites
 #'
 #' @param path [path] directory to create
-#' @param RECURSIVE [lgl] dir.create arg (default = TRUE)
+#' @param recursive [lgl] dir.create arg (default = TRUE)
 #'
 #' @return [lgl] did directory creation succeed or fail?
 #' @export
-make_directory <- function(path, RECURSIVE = TRUE) {
-  dir.create(path, recursive = RECURSIVE, showWarnings = FALSE)
+make_directory <- function(path, recursive = TRUE) {
+  dir.create(path, recursive = recursive, showWarnings = FALSE)
 }
 
 #' Current Date-Time Stamp
