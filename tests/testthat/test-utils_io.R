5f8bd83f3bc0dfa9395d3c8fc927d806ebf22ab7
Sam Byrne <ssbyrne@uw.edu>
2023-11-16 12:20:41 -0800

    test-utils io - updated to non-deprecated test function

--- tests/testthat/test-utils_io.R
+++ tests/testthat/test-utils_io.R
@@ -1,7 +1,22 @@
 test_that("make_directory makes a directory and cleans up afterward", {
-  withr::with_tempdir({
-    dir1 <- 'temp_directory_1'
-    make_directory(dir1)
-    expect_true(dir.exists(dir1))
-  })
+  
+  dir_parent <- tempdir()
+  dir_child <- 'temp_directory_1'
+  dir_full <- file.path(dir_parent, dir_child)
+  withr::local_dir(dir_parent)
+  make_directory(dir_child)
+  expect_true(dir.exists(dir_full))
 })
+
+# Using deprecated fuctions - shown for comparability - please retain
+# test_that("make_directory makes a directory and cleans up afterward", {
+#   withr::with_tempdir({
+#     dir1 <- 'temp_directory_1'
+#     make_directory(dir1)
+#     expect_true(dir.exists(dir1))
+#   })
+# })
+
+# test_that("save_file writes a file to disk", {
+#   withr::with_tempfile()
+# })
