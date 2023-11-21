76b293fed56ebee1582ba9628b402caf402cf4e1
Sam Byrne <ssbyrne@uw.edu>
2023-11-21 09:40:55 -0800

    adding tests for demo PR

--- tests/testthat/test-utils_io.R
+++ tests/testthat/test-utils_io.R
@@ -1,12 +1,14 @@
 
 # Common test cruft
 
+# directories
 dir_parent  <- tempdir()
 dir_child   <- 'temp_directory_1'
 dir_full    <- file.path(dir_parent, dir_child)
-save_object <- list(a = 1, b = 2, c = 3)
-fpath_csv   <- file.path(dir_parent, 'save_object.csv')
-fpath_h5    <- file.path(dir_parent, 'save_object.h5')
+# objects to read and write
+save_object             <- list(a = 1, b = 2, c = 3)
+fpath_supported_ftype   <- file.path(dir_parent, 'save_object.csv')
+fpath_unsupported_ftype <- file.path(dir_parent, 'save_object.h5')
 
 # first enforce the expected tempdir does not exist before tests
 # - /tmp is node specific AND
@@ -26,7 +28,8 @@ test_that("make_directory makes a directory and cleans up afterward",
 test_that("directory is actually gone",
           {
             expect_false(dir.exists(dir_parent))
-          })
+          }
+)
 
 # Using deprecated fuctions - shown for comparability - please retain
 # test_that("make_directory makes a directory and cleans up afterward", {
@@ -42,10 +45,10 @@ test_that("save_file writes a file for a correct extension",
             withr::local_file(dir_parent)
             dir.create(dir_parent)
             save_file(object = save_object
-                      , f_path = fpath_csv
+                      , f_path = fpath_supported_ftype
                       , forbid_overwrite = TRUE
                       , verbose = FALSE)
-            expect_true(file.exists(fpath_csv))
+            expect_true(file.exists(fpath_supported_ftype))
           }
 )
 
@@ -56,23 +59,60 @@ test_that("save_file forbids overwrite",
             dir.create(dir_parent)
             
             save_file(object = save_object
-                      , f_path = fpath_csv
+                      , f_path = fpath_supported_ftype
                       , forbid_overwrite = TRUE
                       , verbose = FALSE)
             
             expect_message(
               save_file(object = save_object
-                        , f_path = fpath_csv
+                        , f_path = fpath_supported_ftype
                         , forbid_overwrite = TRUE
                         , verbose = FALSE)
-              , regexp = paste("File already exists, not over-writing :", fpath_csv)
+              , regexp = paste("File already exists, not over-writing :", fpath_supported_ftype)
               
             ) 
             
           }
 )
 
+test_that("save_file errors correctly for nonexistent directory",
+          {
+            expect_error(
+              save_file(object = save_object
+                        , f_path = fpath_supported_ftype
+                        , forbid_overwrite = TRUE
+                        , verbose = FALSE)
+              , regexp = "Parent directory does not exist, please create it first"
+              
+            )
+          })
+
+test_that("save_file prevents saving files with unsupported extension", 
+          {
+            withr::local_file(dir_parent)
+            dir.create(dir_parent)
+            expect_error(
+              save_file(object = save_object
+                        , f_path = fpath_unsupported_ftype
+                        , forbid_overwrite = TRUE
+                        , verbose = FALSE)
+              , regexp = "This function only supports .* file extensions \\(case-insensitive\\)"
+              
+            )
+            expect_false(file.exists(fpath_unsupported_ftype))
+          }
+)
+
+test_that("save_file produces correct messages",
+          {
+            
+          }
+)
+
+
+# Last test
 test_that("directory is actually gone",
           {
             expect_false(dir.exists(dir_parent))
-          })
+          }
+)
