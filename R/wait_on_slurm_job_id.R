c3df073228341196c2b6db9dc8f3cbb3ceae94ed
Sam Byrne <ssbyrne@uw.edu>
2023-12-29 16:37:29 -0800

    wait on job ids - adjusted user feedback to give relevant info regardless of how they filter

--- R/wait_on_slurm_job_id.R
+++ R/wait_on_slurm_job_id.R
@@ -51,7 +51,11 @@
 #' @param filter_regex [regex] required if `filter_by %in% c("JobName",
 #'   "Account")`
 #' @param break_on_failure [lgl] if _any_ of your jobs fail, should this
-#'   function break?
+#'   function break?  Failure itself is always determined based on the user's
+#'   filters, but failure _feedback_ always returns JobID, regardless of
+#'   filtering. This is due to how jobs are initially queried.  This may
+#'   included unwanted recycled `JobID`s, and it is **_up to the user_** to
+#'   determine which are relevant to their work.
 #' @param dryrun [lgl] return a list of commands built by this function, but do
 #'   not wait on jobs (only returns command for the first batch - see
 #'   `batch_size`)
@@ -76,16 +80,17 @@ wait_on_slurm_job_id <-
     
     # sacct formatting 
     format_list <- list(
-      state      = "State%16"
-      , jobid    = "JobID%20"
-      , jobidraw = "JobIDRaw%20"
-      , user     = "User%12"
-      , jobname  = "JobName%50"
-      , account  = "Account%20"
+      state    = "State%16",
+      jobid    = "JobID%20",
+      jobidraw = "JobIDRaw%20",
+      user     = "User%12",
+      jobname  = "JobName%50",
+      account  = "Account%20"
     )
     names(format_list) <- tolower(names(format_list))
-    formatting_str    <- paste0("--format=", paste(format_list, collapse = ","))
     if(!names(format_list)[[1]] == "state") stop("state must be first `format_list` object element")
+    if(!names(format_list)[[2]] == "jobid") stop("jobid must be second `format_list` object element")
+    formatting_str    <- paste0("--format=", paste(format_list, collapse = ","))
     
     irrelevant_fields <- c("state", "jobid")
     regex_req_fields  <- c("jobname", "account")
@@ -94,7 +99,7 @@ wait_on_slurm_job_id <-
     valid_filters <- setdiff(names(format_list), irrelevant_fields)
     
     # argument validation
-    if(!all(filter_by %in% valid_filters)) stop("filter_by must be one of: ", paste(c(valid_filters, "NULL"), collapse = ", "))
+    if(!all(filter_by %in% valid_filters)) stop("filter_by must be one of: ", paste(c(valid_filters, "(case-insensitive) or NULL"), collapse = ", "))
     if(is.null(filter_regex) && any(regex_req_fields %in% filter_by)) stop("must define filter_regex for: ",
                                                                            paste(regex_req_fields, collapse = ", "))
     regex_user_fields <- filter_by[filter_by %in% regex_req_fields]
@@ -109,7 +114,7 @@ wait_on_slurm_job_id <-
     # Break job_id into batches
     batches_n <- ceiling(length(job_id)/batch_size)
     if(max(batches_n) > 1) print(paste("Warning: You have submitted more than", batch_size, "JobIDs." , 
-                                 "Waiting for all jobs in batch_idx 1 before proceeding to batch_idx 2, etc. (grep limitation)"))
+                                       "Waiting for all jobs in batch_idx 1 before proceeding to batch_idx 2, etc. (grep limitation)"))
     
     # Loop over the jobs in batches_n (too may job_ids leads to grep errors)
     for(batch_idx in 1:batches_n){
@@ -121,7 +126,7 @@ wait_on_slurm_job_id <-
         last  = min((batch_idx * batch_size), length(job_id))
       )
       job_id_batch  <- job_id[job_idx$first : job_idx$last]
-
+      
       job_id_regex_raw          <- paste(job_id_batch, collapse = "|")
       job_id_regex_or_quoted    <- paste0("'", job_id_regex_raw, "'")
       job_id_regex_comma_quoted <- paste0("'", paste(job_id, collapse = ","), "'")
@@ -130,12 +135,12 @@ wait_on_slurm_job_id <-
       filter_regex_list <- lapply(filter_by, function(flt){
         switch(
           flt
-          , "state"    = "'.*'" # filter nothing - handles filter_by = NULL
-          # , "jobid"    = job_id_regex_or_quoted # has no meaning - we're querying JobID directly
-          , "jobidraw" = job_id_regex_or_quoted
-          , "user"     = Sys.info()[["user"]]
-          , "jobname"  = filter_regex
-          , "account"  = filter_regex
+          "state"    = "'.*'", # filter nothing - handles filter_by = NULL
+          # "jobid"    = job_id_regex_or_quoted, # has no meaning - we're querying JobID directly
+          "jobidraw" = job_id_regex_or_quoted,
+          "user"     = Sys.info()[["user"]],
+          "jobname"  = filter_regex,
+          "account"  = filter_regex
         )
       })
       names(filter_regex_list) <- filter_by
@@ -163,21 +168,30 @@ wait_on_slurm_job_id <-
         filter_regex
       )
       
+      # relevant if user does not filter by JobIDRaw (see `break_for_failed_jobs()`)
+      filter_str_fail_feedback <- paste0(
+        "| tail -n +3 ",
+        "| grep -vP 'batch|extern' ",
+        "| sed 's/ \\+/ /g; s/^[[:space:]]*//; s/[[:space:]]*$//' ", 
+        "| cut -d' ' -f", "1,2"
+      )
+      
       # Build submission commands
       cmd_base <- paste("sacct -j", job_id_regex_comma_quoted, formatting_str, filter_str)
       cmd_pass <- paste0(cmd_base, " | grep -P 'RUNNING|PENDING'")
       cmd_fail <- paste0(cmd_base, " | grep -P 'FAILED'")
+      # need job_id included for failure feedback
+      cmd_fail_feedback <-paste("sacct -j",
+                                job_id_regex_comma_quoted,
+                                formatting_str,
+                                filter_str_fail_feedback,
+                                "| grep -P 'FAILED'")
       
-      if(dryrun) return(list(cmd_base = cmd_base, cmd_pass = cmd_pass, cmd_fail = cmd_fail))
-      
+      if(dryrun) return(list(cmd_base = cmd_base, cmd_pass = cmd_pass, cmd_fail = cmd_fail, cmd_fail_feedback = cmd_fail_feedback))
       if(!length(suppressWarnings(system(cmd_base, intern = TRUE)))) stop ("No jobs found: ", cmd_base)
       
       # Stop for immediately FAILED jobs
-      if(break_on_failure){
-        fail_chk    <- suppressWarnings(system(cmd_fail, intern = TRUE))
-        failed_jobs <- paste(unique(regmatches(fail_chk, regexpr(job_id_regex_raw, fail_chk))), collapse = ", ")
-        if(length(fail_chk)) stop('there is a failure among the launched jobs, investigate:\n', failed_jobs)
-      }
+      if(break_on_failure) break_for_failed_jobs(cmd_fail, cmd_fail_feedback, job_id_regex_raw, filter_by)
       
       print("seconds elapsed:")
       
@@ -185,13 +199,8 @@ wait_on_slurm_job_id <-
       while(length(suppressWarnings(system(cmd_pass, intern = T))) > 0 ) {
         Sys.sleep(cycle_sleep_sec)
         print(round((proc.time() - start.time)[[3]],0))
-        
         # Stop if any jobs have FAILED State
-        if(break_on_failure){
-          fail_chk    <- suppressWarnings(system(cmd_fail, intern = TRUE))
-          failed_jobs <- paste(unique(regmatches(fail_chk, regexpr(job_id_regex_raw, fail_chk))), collapse = ", ")
-          if(length(fail_chk)) stop('there is a failure among the launched jobs, investigate:\n', failed_jobs)
-        } 
+        if(break_on_failure) break_for_failed_jobs(cmd_fail, cmd_fail_feedback, job_id_regex_raw, filter_by)
       }
       
     }
@@ -203,4 +212,31 @@ wait_on_slurm_job_id <-
     # Complete
     job_id_msg <- paste(job_id, collapse = ", ")
     print(paste0("Job(s) ", job_id_msg, " no longer PENDING, RUNNING, or FAILED. Time elapsed: ", job.runtime, " seconds"))
+  }
+
+#' @param cmd_fail [chr]
+#' @param cmd_fail_feedback [chr]
+#' @param job_id_regex_raw [regex]
+#' @param filter_by [chr]
+#'
+#' @description Helper function for wait_on_slurm_job_id - how do you want jobs to break and display user messages?
+break_for_failed_jobs <-
+  function(
+    cmd_fail, 
+    cmd_fail_feedback, 
+    job_id_regex_raw, 
+    filter_by
+  ) {
+    # if user is filtering on JobIDRaw, return relevant jobs and feedback, otherwise return all JobIDs found by initial Slurm query
+    jobid_present     <- any(grepl("jobidraw", filter_by))
+    failure_message   <- ifelse(jobid_present, 
+                                "There is a failure among the launched jobs, investigate.\n", 
+                                "There is a failure among the launched jobs, investigate. (NOTE! May include recycled JobIDs)\n")
+    cmd_fail_feedback <-ifelse(jobid_present,
+                               cmd_fail,
+                               cmd_fail_feedback)
+    fail_chk          <- suppressWarnings(system(cmd_fail, intern = TRUE))
+    fail_feedback     <- suppressWarnings(system(cmd_fail_feedback, intern = TRUE))
+    failed_jobs       <- paste(unique(regmatches(fail_feedback, regexpr(job_id_regex_raw, fail_feedback))), collapse = ", ")
+    if(length(fail_chk)) stop(failure_message, failed_jobs)
   }
