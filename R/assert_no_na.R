59402406131f4a252ea2b40fad526fbc132a3680
Sam Byrne <ssbyrne@uw.edu>
2025-04-01 11:16:12 -0700

    update - assert_no_na - speed up implementation if no NA found anywhere in selected columns

--- R/assert_no_na.R
+++ R/assert_no_na.R
@@ -12,10 +12,13 @@ assert_no_na <- function(dt, varnames, verbose = TRUE){
   if(!is.character(varnames)) stop("varnames must be a character vector")
   if(!all(varnames %in% names(dt))) stop("Not all varnames are present in the data.table")
 
-  na_rows <- dt[dt[, .I[apply(.SD, 1, anyNA)], .SDcols = varnames]]
+  # Rowwise checks are slow - only perform full checks if any NA are found
+  if (anyNA(dt[, ..varnames])) {
+    na_rows <- dt[dt[, .I[apply(.SD, 1, anyNA)], .SDcols = varnames]]
 
-  if(nrow(na_rows) > 0) {
-    stop("NA values found in the following rows: ", toString(paste(names(na_rows), na_rows[1, ], sep = ":") ))
+    if(nrow(na_rows) > 0) {
+      stop("NA values found - example row: ", toString(paste(names(na_rows), na_rows[1, ], sep = ":") ))
+    }
   }
 
   if(verbose) message("No NA values found in: ", toString(varnames))
