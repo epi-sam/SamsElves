c63100e71df357cd0f97833d9d48285f738598e6
Sam Byrne <ssbyrne@uw.edu>
2026-01-09 14:20:00 -0800

    assert no na -default to not verbose

--- R/assert_no_na.R
+++ R/assert_no_na.R
@@ -7,7 +7,7 @@
 #' @return [none] stop if NA values found in `varnames`
 #' @export
 #'
-assert_no_na <- function(dt, varnames = names(dt), verbose = TRUE){
+assert_no_na <- function(dt, varnames = names(dt), verbose = FALSE){
   if(!data.table::is.data.table(dt)) stop("dt must be a data.table")
   if(!is.character(varnames)) stop("varnames must be a character vector")
   if(!all(varnames %in% names(dt))) stop("Not all varnames are present in the data.table")
